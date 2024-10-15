785a01f69e9f8c2cefa5ab0a682a15213c06539d
Ambreen-Fatima216 <ambreen2007.216@gmail.com>
2024-10-15 13:07:28 +0400

    Exercise 01
    Coding is cool

---
+++ Exercise 01/Coding is cool.py
@@ -0,0 +1,7 @@
+word_1 = 'Coding ' #first variable
+word_2 = 'is '     #Second variable
+word_3 = 'cool '   #Third variable
+
+# printing the output using string concatination
+message= word_1 + word_2 +word_3
+print(message) # printing the output using string concatination
