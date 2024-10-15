6455989f031dca9a4e5b03a5d6a10c78d529573b
Ambreen-Fatima216 <ambreen2007.216@gmail.com>
2024-10-15 13:14:26 +0400

    Exercise 02
    Simple sums

---
+++ Exercise 02/Simple sums.py
@@ -0,0 +1,5 @@
+integer_1 = 8 #Integer variable 1
+integer_2 = 10#Integer variable 2
+integer_3 = integer_1 + integer_2 #Sum of integer_1 and integer_2
+print(integer_3) #printing the output of the sum
+
