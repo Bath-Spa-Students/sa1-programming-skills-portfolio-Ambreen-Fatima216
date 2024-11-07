0fee39cb6915c6c13034da004148e19d7ba31f6b
Ambreen-Fatima216 <ambreen2007.216@gmail.com>
2024-11-08 01:39:57 +0400

    Biography Simple

--- Exercise 03/Biography simple.py
+++ Exercise 03/Biography simple.py
@@ -4,13 +4,14 @@ In this exercise, you'll create a program that stores and prints your name, home
 
 ### Steps:
 1. Store the information (name, hometown, and age) as key-value pairs in a dictionary.
-2. Print the values on separate lines using a single `print()` statement.
+2. Print the values on separate  lines using a single `print()` statement.
 3. Use variables with appropriate data types for each piece of information."""
 
 #Storing information inside dictionaries
-Personal_Information = {
+Personal_Information = { 
     'name': 'Ambreen Fatima Naqvi',  # Name Key Variable
-    'hometown': 'Sharjah, UAE',  # Hometown Key variable
+    'hometown': 'Sharjah, UAE',   # Hometown Key variable
     'age': 17  #Age Key variable
 }
 print("Name: " + Personal_Information['name'] + "\nHometown: " + Personal_Information['hometown'] + "\nAge: " + str(Personal_Information['age']))
+#The print statement uses string concatenation to output on seperate lines 
