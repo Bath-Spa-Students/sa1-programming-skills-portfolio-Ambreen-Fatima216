25d011901af93e83901299864da34c47e38e3de1
Ambreen-Fatima216 <ambreen2007.216@gmail.com>
2024-11-13 21:41:16 +0400

    Is it even.py

--- Is it even.py
+++ Is it even.py
@@ -4,7 +4,7 @@ Write a program that tests if a value is even or odd. Follow the instructions ou
 ### Instructions:
 * The program should ask the user for a number from within the main function.
 * The entered number should be passed to a function that determines if the value is even or odd.
-* The function should return a message indicating whether the number is even or odd.
+* The function should return a message indicating whether the number is even or odd. 
 * The message returned by the function should be printed from within the main function.
 """
 # Function to check whether the number is odd or even(Concepy for this learned from:https://www.toppr.com/guides/python-guide/examples/python-examples/python-program-to-check-if-a-number-is-odd-or-even/#:~:text=Also%2C%20if%E2%80%A6else%20statements%20will,required%20code%20is%20provided%20below.&text=num%20%3D%20int%20(input%20(%E2%80%9C,even%3A%20887%20887%20is%20odd. )
