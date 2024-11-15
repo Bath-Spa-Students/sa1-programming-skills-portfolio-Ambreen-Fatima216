a14b9e527068706344b91af559b3f8ac4b642292
Ambreen-Fatima216 <ambreen2007.216@gmail.com>
2024-11-16 02:34:48 +0400

    Simple sums

--- Exercise 02/Simple sums.py
+++ Exercise 02/Simple sums.py
@@ -9,7 +9,7 @@ In this exercise, you will create and work with integer variables, perform arith
 """
 #assigning vaules to the variablesS
 integer_1 = 8 #Integer variable 1
-integer_2 = 10#Integer variable 2
+integer_2 = 10#Integer variable 2 
 integer_3 = integer_1 + integer_2 #Sum of integer_1 and integer_2
 
 print(integer_3) #printing the output of the sum
