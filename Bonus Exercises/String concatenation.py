c44609d9cf1c2b83d271d7bcf4e3987768caea94
Ambreen-Fatima216 <ambreen2007.216@gmail.com>
2024-11-17 00:04:35 +0400

    String Concatenation Bonus

---
+++ Bonus Exercises/String concatenation.py
@@ -0,0 +1,8 @@
+"""Strings Concatenation:
+Write three strings in different variables and print the output as one string."""
+String_1 = "She sells " #Variable 1
+String_2 = "sea shells "  #Variable 2
+String_3 = "on the sea shore." #Variable 3
+
+Message_output =  String_1 + String_2 + String_3 # Variable that holds the concatenation
+print(Message_output) #Printing the output of the concatenation
