ba470ec1bad9934710c52ffaec107eef9daf4592
Ambreen-Fatima216 <ambreen2007.216@gmail.com>
2024-11-17 00:10:35 +0400

    Variables Bonus

---
+++ Bonus Exercises/Variables Bonus.py
@@ -0,0 +1,8 @@
+"""Variables:
+Assign a message to a variable, and print that message. Then change the value of the
+variable to a new message, and print the new message."""
+
+Tongue_twister = "A quick brown fox."
+print(Tongue_twister)
+Tongue_twister = "A quick brown fox jumped over a lazy dog."
+print(Tongue_twister)
